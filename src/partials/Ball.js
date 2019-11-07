f1dbb8786e997a4b73daf733865fc78598e08f38
DCCA <dcca12@gmail.com>
2019-11-06 20:27:38 -0800

    add ball first movements

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -1,12 +1,27 @@
 import { SVG_NS, BALL_COLOR } from '../settings';
 
 export default class Ball {
-    constructor(radius, x, y, speed) {
+    constructor(radius, boardWidth, boardHeight, speed) {
       this.radius = radius;
-      this.x = x;
-      this.y = y;
+      this.boardWidth = boardWidth;
+      this.boardHeight = boardHeight;
+      this.direction = 1;
       this.speed = speed;
-    
+      this.reset();
+    }
+    ballMove(){
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
+    }
+
+    reset(){
+        this.x = this.boardWidth/2;
+        this.y = this.boardHeight/2;
+        this.vy = 0;
+        while(this.vy === 0) {
+            this.vy = Math.floor((Math.random() * 10) -5);
+        };
+        this.vx = this.direction * (6 - Math.abs(this.vy));
     }
   
     render(svg) {
@@ -18,5 +33,7 @@ export default class Ball {
       ball.setAttributeNS(null, "fill", BALL_COLOR);
 
       svg.appendChild(ball);
+
+      this.ballMove();
     }
   }
